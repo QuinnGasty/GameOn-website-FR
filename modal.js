708eb3afc60d5d3818a26f346e6eb376d04376ed
Marjorie Esnault <gastyquinn@gmail.com>
2021-08-09 16:04:18 +0200

    Validation mail / date / quantity

--- modal.js
+++ modal.js
@@ -16,8 +16,8 @@ const form = document.querySelector('#subscriptionform')
 
 // RegEx
 const regExEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$','g');
-const regExDate = new RegExp('^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)([1930-2011]){4}$', 'g');
-const regExNb = new RegExp(/^[0-9]+$/, 'g');
+const regExBirthdate = new RegExp(/^((19[3-9]+[0-9]|200[0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](19[3-9]+[0-9]|200[0-6]))$/);
+const regExNb = new RegExp(/^([0-9]){1,2}$/);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -32,6 +32,7 @@ modalBtnClose.addEventListener ("click", () => {
   modalbg.style.display = "none";
 })
 
+// name and surname validation
 const validtext = function(inputid, info) {
   let msg;
   let valid = false;
@@ -64,6 +65,74 @@ const validtext = function(inputid, info) {
   form.last.addEventListener("change", () => {
     validtext(last, infolast);
   });
+
+  // email validation
+  const validEmail = function(inputEmail, info) {
+    let msg;
+    let valid = false;
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(inputEmail.value)) {
+      msg = 'Adresse valide';
+      info.classList.remove('error');
+      info.classList.add('no-error');
+      valid = true;
+    } else {
+      msg = 'Adresse non valide';
+      info.classList.remove('no-error');
+      info.classList.add('error');
+    } 
+    info.textContent = msg;
+    return valid;
+  };
+
+  form.email.addEventListener("change", () => {
+    validEmail(email, infoemail);
+  });
+
+  // birthdate validation
+  const validDate = function(inputDate, info) {
+    let msg;
+    let valid = false;
+    if (regExBirthdate.test(inputDate.value)) {
+      msg = 'Date valide'
+      info.classList.add('no-error');
+      info.classList.remove('error');
+      valid = true;
+    } else {
+      msg = 'Date non valide';
+      info.classList.remove('no-error');
+      info.classList.add('error');
+    }
+    info.textContent = msg;
+    return valid;
+  }
+
+  form.birthdate.addEventListener("change", () => {
+    validDate(birthdate, infodate);
+  })
+
+  //  number of tournaments validation
+  const validNumber = function(inputNumber, info) {
+    let msg;
+    let valid = false;
+    if (regExNb.test(inputNumber.value)) {
+      msg = 'Valide'
+      info.classList.add('no-error');
+      info.classList.remove('error');
+      valid = true;
+    } else {
+      msg = 'Ce champs doit comporter une valeur numérique comprise entre 0 et 99';
+      info.classList.add('error');
+      info.classList.remove('no-error');
+    }
+    info.textContent = msg;
+    return valid;
+  }
+
+  form.quantity.addEventListener("change", () => {
+    validNumber(quantity, infonumber);
+  })
+
+  // 
 
   //function addition(a,b) {
     //console.log(`${a} + ${b} = ${a+b}`);
